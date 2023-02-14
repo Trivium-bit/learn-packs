02ad544c060f71930054f7de6976c5b9114c2cb2
Trivium-bit <trivium.exe@gmail.com>
2023-02-14 12:56:32 +0300

    feat: add UseDebouce for Search CardsPack

--- src/feature/packsList/SortOptions/SearchPack.tsx
+++ src/feature/packsList/SortOptions/SearchPack.tsx
@@ -1,17 +1,18 @@
 import { Box, TextField } from "@mui/material"
-import { ChangeEvent } from "react";
-import { findNameCardsPackAC } from "redux/packs-reducer";
+import useDebounce from "components/UseDebounce/UseDebounce";
+import { ChangeEvent, useEffect, useState } from "react";
+import { findNameCardsPackAC, getCardsPacksTC } from "redux/packs-reducer";
 import { useAppDispatch } from "redux/store";
 import s from "./NameOptions.module.css";
 
 
-export const SearchPack: React.FC = () => {
+/* export const SearchPack: React.FC = () => {
     
     const dispatch = useAppDispatch();
-
+     
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         dispatch(findNameCardsPackAC(e.target.value))
-    }
+    }   
 
     return (
         <Box 
@@ -27,4 +28,31 @@ export const SearchPack: React.FC = () => {
             />
         </Box>
     )
+} */
+
+export default function SearchPack() {
+
+  const [searchValue, setSearchValue] = useState<string>('')
+  const debouncedValue = useDebounce<string>(searchValue, 500)
+
+  const dispatch = useAppDispatch();
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    
+    setSearchValue(event.target.value)
+    dispatch(findNameCardsPackAC(searchValue))
+  }
+
+  // Fetch API (optional)
+  useEffect(() => {
+    dispatch(findNameCardsPackAC(searchValue))
+  }, [debouncedValue])
+
+  return (
+    <div>
+      <p>Value real-time: {searchValue}</p>
+      <p>Debounced value: {debouncedValue}</p>
+      <input className={s.titleName} type="text" value={searchValue} onChange={handleChange} />
+    </div>
+  )
 }
--- src/feature/packsList/SortOptions/SortOptions.tsx
+++ src/feature/packsList/SortOptions/SortOptions.tsx
@@ -1,7 +1,8 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import { MyAllPacks } from './MyAllPacks'
-import { SearchPack } from './SearchPack'
+import SearchPack from './SearchPack'
+
 
 
 export const SortOptions: React.FC = () => {
